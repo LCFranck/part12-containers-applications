a6d83c2bcef21287234a4bda6103255346d23ba2
LCFranck <cassandra.franck@gmail.com>
2026-02-16 17:47:21 +0200

    12.12 done

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,6 +1,7 @@
+const redis = require('redis')
 const express = require('express');
 const router = express.Router();
-
+const { getAsync } = require('../redis');
 const configs = require('../util/config')
 
 let visits = 0
@@ -12,6 +13,14 @@ router.get('/', async (req, res) => {
   res.send({
     ...configs,
     visits
+  });
+});
+
+router.get('/statistics', async (req, res) => {
+  const added_todos = await getAsync('added_todos');
+  
+  res.send({
+    added_todos: added_todos ? parseInt(added_todos) : 0
   });
 });
 
